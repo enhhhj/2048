1bca1b90c78b8c2f1969c48ff9b60d4666120b57
EarisHu <12311047@mail.sustech.edu.cn>
2024-05-22 23:11:00 +0800

    增加了随机添加数字

--- model/GridNumber.java
+++ model/GridNumber.java
@@ -9,7 +9,7 @@ public class GridNumber {
     private final int X_COUNT;
     private final int Y_COUNT;
 
-    private int[][] numbers;
+    private int [][] numbers;
 
     static Random random = new Random();
 
@@ -23,13 +23,24 @@ public class GridNumber {
     public void initialNumbers() {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
-                //todo: update generate numbers method
-                numbers[i][j] = random.nextInt(2) == 0 ? 1 : 0;
+                this.numbers[i][j]=0;
             }
         }
+        int a = random.nextInt(numbers.length) + 0;
+        int b = random.nextInt(numbers[a].length) + 0;
+        int c =random.nextInt(2) + 1;
+        if(c==1){
+            numbers[a][b] = 2;
+        }else{
+            numbers[a][b] = 4;
+        }
+                //todo: update generate numbers method
     }
+
+
     //todo: finish the method of four direction moving.
     public void moveRight() {
+        int [][] A = this.numbers;
         for (int i =0; i<numbers.length; i++) {
             int[] newRow = new int[numbers[i].length];
             ArrayList<Integer> news = new ArrayList<>();
@@ -43,33 +54,62 @@ public class GridNumber {
                 news.add(newRow[u]);
             }}
 
-            if(news.size() >1){
-            for(int m=0;m<news.size()-1;m++){
-                if(news.get(m)==news.get(m+1)){
-                    news1.add(2*news.get(m));
-                    news.set(m,0);
-                    news.set(m+1,0);
+            if(news.size()>1){
+                for(int m=0;m<news.size()-1;m++){
+                    if(news.get(m)==news.get(m+1)){
+                        news1.add(2*news.get(m));
+                        news.set(m,0);
+                        news.set(m+1,0);
+                    }
+                    if(news.get(m)!=news.get(m+1) && news.get(m)!=0 ) {
+                        news1.add(news.get(m));
+                    }
                 }
-                if(news.get(m)!=news.get(m+1)&&news.get(m)!=0) {
-                    news1.add(news.get(m));
-                }}if(news.get(news.size()-1)!=news.get(news.size()-2))
+                if(news.get(news.size()-1)!=0){
                     news1.add(news.get(news.size()-1));
-                                }
-            if(news.size()==1){news1.add(news.get(0));}
-
-
+                }
+            }
+            if(news.size()==1){
+                news1.add(news.get(0));
+            }
             int r=numbers[i].length-news1.size();
             for(int c=0;c<r;c++)
             {
                 news1.add(0);
             }
-
-            for(int n=0;n<numbers[i].length;n++)
-            {numbers[i][n]=news1.get(numbers[i].length-n-1);
-          }}}
+            for(int n=0;n<numbers[i].length;n++) {
+                numbers[i][n]=news1.get(numbers[i].length-n-1);
+            }
+        }
+        if(numbers.equals(A)) {
+            //steps++;
+            int a = random.nextInt(numbers.length) + 0;
+            int b = random.nextInt(numbers[a].length) + 0;
+            if(numbers[a][b]==0){
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }else{
+                while(numbers[a][b]!=0){
+                    a = random.nextInt(numbers.length) + 0;
+                    b = random.nextInt(numbers[a].length) + 0;
+                }
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }
+        }
+    }
 
 
     public void moveLeft() {
+        int [][] A = this.numbers;
         for (int i =0; i<numbers.length; i++) {
             int[] newRow = new int[numbers[i].length];
             ArrayList<Integer> news = new ArrayList<>();
@@ -92,10 +132,15 @@ public class GridNumber {
                     }
                     if(news.get(m)!=news.get(m+1)&&news.get(m)!=0) {
                         news1.add(news.get(m));
-                    }}if(news.get(news.size()-1)!=news.get(news.size()-2))
+                    }
+                }
+                if(news.get(news.size()-1)!=0){
                     news1.add(news.get(news.size()-1));
+                }
             }
-            if(news.size()==1){news1.add(news.get(0));}
+            if(news.size()==1){
+                news1.add(news.get(0));
+            }
 
 
             int r=numbers[i].length-news1.size();
@@ -106,9 +151,37 @@ public class GridNumber {
 
             for(int n=0;n<numbers[i].length;n++)
             {numbers[i][n]=news1.get(n);
-            }}}
+            }
+        }
+        if(numbers.equals(A)) {
+            //steps++;
+            int a = random.nextInt(numbers.length) + 0;
+            int b = random.nextInt(numbers[a].length) + 0;
+            if(numbers[a][b]==0){
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }else{
+                while(numbers[a][b]!=0){
+                    a = random.nextInt(numbers.length) + 0;
+                    b = random.nextInt(numbers[a].length) + 0;
+                }
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }
+        }
+    }
+
 
     public void moveUp() {
+        int [][] A = this.numbers;
         for (int i =0; i<numbers[0].length; i++) {
             int[] newRow = new int[numbers[0].length];
             ArrayList<Integer> news = new ArrayList<>();
@@ -122,32 +195,61 @@ public class GridNumber {
                     news.add(newRow[u]);
                 }}
 
-            if(news.size() >1){
-                for(int m=0;m<news.size()-1;m++){
-                    if(news.get(m)==news.get(m+1)){
-                        news1.add(2*news.get(m));
-                        news.set(m,0);
-                        news.set(m+1,0);
+            if(news.size() >1) {
+                for (int m = 0; m < news.size() - 1; m++) {
+                    if (news.get(m) == news.get(m + 1)) {
+                        news1.add(2 * news.get(m));
+                        news.set(m, 0);
+                        news.set(m + 1, 0);
                     }
-                    if(news.get(m)!=news.get(m+1)&&news.get(m)!=0) {
+                    if (news.get(m) != news.get(m + 1) && news.get(m) != 0) {
                         news1.add(news.get(m));
-                    }}if(news.get(news.size()-1)!=news.get(news.size()-2))
-                    news1.add(news.get(news.size()-1));
+                    }
+                }
+                if (news.get(news.size() - 1) != 0) {
+                    news1.add(news.get(news.size() - 1));
+                }
             }
             if(news.size()==1){news1.add(news.get(0));}
 
 
             int r=numbers[i].length-news1.size();
-            for(int c=0;c<r;c++)
-            {
+            for(int c=0;c<r;c++) {
                 news1.add(0);
             }
 
-            for(int n=0;n<numbers[i].length;n++)
-            {numbers[n][i]=news1.get(n);
-            }}}
+            for(int n=0;n<numbers[i].length;n++) {
+                numbers[n][i]=news1.get(n);
+            }
+        }
+        if(numbers.equals(A)) {
+            //steps++;
+            int a = random.nextInt(numbers.length) + 0;
+            int b = random.nextInt(numbers[a].length) + 0;
+            if(numbers[a][b]==0){
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }else{
+                while(numbers[a][b]!=0){
+                    a = random.nextInt(numbers.length) + 0;
+                    b = random.nextInt(numbers[a].length) + 0;
+                }
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }
+        }
+    }
 
     public void moveDown() {
+        int [][] A = this.numbers;
         for (int i =0; i<numbers.length; i++) {
             int[] newRow = new int[numbers[0].length];
             ArrayList<Integer> news = new ArrayList<>();
@@ -170,7 +272,7 @@ public class GridNumber {
                     }
                     if(news.get(m)!=news.get(m+1)&&news.get(m)!=0) {
                         news1.add(news.get(m));
-                    }}if(news.get(news.size()-1)!=news.get(news.size()-2))
+                    }}if(news.get(news.size() - 1) != 0)
                     news1.add(news.get(news.size()-1));
             }
             if(news.size()==1){news1.add(news.get(0));}
@@ -184,13 +286,33 @@ public class GridNumber {
 
             for(int n=0;n<numbers[i].length;n++)
             {numbers[n][i]=news1.get(numbers[i].length-n-1);
-            }}}
-
-
-
-
-
-
+            }
+        }
+        if(numbers.equals(A)) {
+            //steps++;
+            int a = random.nextInt(numbers.length) + 0;
+            int b = random.nextInt(numbers[a].length) + 0;
+            if(numbers[a][b]==0){
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }else{
+                while(numbers[a][b]!=0){
+                    a = random.nextInt(numbers.length) + 0;
+                    b = random.nextInt(numbers[a].length) + 0;
+                }
+                int c =random.nextInt(2) + 1;
+                if(c==1){
+                    numbers[a][b] = 2;
+                }else{
+                    numbers[a][b] = 4;
+                }
+            }
+        }
+    }
 
 
 
