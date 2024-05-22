85f6b14720d304f479ca139f791f656d6f1a0640
张笑宇 <15838710256@163.com>
2024-05-22 23:05:56 +0800

    加了注册登录的页面 还有一部分的游戏界面

---
+++ controller/RegisterContorller.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterContorller {
+
+    @FXML
+    private Button Button_register;
+
+    @FXML
+    private Label Label_password;
+
+    @FXML
+    private Label Label_username;
+
+    @FXML
+    private PasswordField PasswordField_password;
+
+    @FXML
+    private TextField TextField_username;
+
+}
